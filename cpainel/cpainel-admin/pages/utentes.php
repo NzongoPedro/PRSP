e963789418b39254f90e867175b388a83e53b3b2
Nzongo Pedro <nzongopedro3@gmail.com>
2023-07-15 18:12:37 +0100

    Card de viazualização de utentes.

--- cpainel/cpainel-admin/pages/utentes.php
+++ cpainel/cpainel-admin/pages/utentes.php
@@ -8,15 +8,108 @@
         </section><!-- Fim utentes -->
 
         <!-- Grelha de utentes -->
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <div class="col">
-                <div class="card h-100">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+            <div class="col" data-aos="fade-up" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="utente-carta carta card h-100 carta-2">
+                    <img src="<?= IMAGENS ?>admin/admin.png" alt="Foto de Perfil" class="shadow-sm profile-picture">
+                    <div class="card-body content">
+                        <h5 class="card-title utente-nome">nome utente</h5>
+                        <div class="traco-2"></div>
+                        <div class="mt-3 mb-3 text-center detalhe">
+                            <a href="#" class="nav-link text-warning">ver mais detalhes</a>
+                        </div>
                     </div>
+                    <div class="shine"></div>
                 </div>
+            </div>
+            <div class="col" data-aos="fade-up" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="utente-carta carta card h-100 carta-2">
+                    <img src="<?= IMAGENS ?>admin/admin.png" alt="Foto de Perfil" class="shadow-sm profile-picture">
+                    <div class="card-body content">
+                        <h5 class="card-title utente-nome">nome utente</h5>
+                        <div class="traco-2"></div>
+                        <div class="mt-3 mb-3 text-center detalhe">
+                            <a href="#" class="nav-link text-warning">ver mais detalhes</a>
+                        </div>
+                    </div>
+                    <div class="shine"></div>
+                </div>
+            </div>
+            <div class="col" data-aos="fade-up" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="utente-carta carta card h-100 carta-2">
+                    <img src="<?= IMAGENS ?>admin/admin.png" alt="Foto de Perfil" class="shadow-sm profile-picture">
+                    <div class="card-body content">
+                        <h5 class="card-title utente-nome">nome utente</h5>
+                        <div class="traco-2"></div>
+                        <div class="mt-3 mb-3 text-center detalhe">
+                            <a href="#" class="nav-link text-warning">ver mais detalhes</a>
+                        </div>
+                    </div>
+                    <div class="shine"></div>
+                </div>
+            </div>
+            <div class="col" data-aos="fade-up" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="utente-carta carta card h-100 carta-2">
+                    <img src="<?= IMAGENS ?>admin/admin.png" alt="Foto de Perfil" class="shadow-sm profile-picture">
+                    <div class="card-body content">
+                        <h5 class="card-title utente-nome">nome utente</h5>
+                        <div class="traco-2"></div>
+                        <div class="mt-3 mb-3 text-center detalhe">
+                            <a href="#" class="nav-link text-warning">ver mais detalhes</a>
+                        </div>
+                    </div>
+                    <div class="shine"></div>
+                </div>
+            </div>
+            <div class="col" data-aos="fade-up" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="utente-carta carta card h-100 carta-2">
+                    <img src="<?= IMAGENS ?>admin/admin.png" alt="Foto de Perfil" class="shadow-sm profile-picture">
+                    <div class="card-body content">
+                        <h5 class="card-title utente-nome">nome utente</h5>
+                        <div class="traco-2"></div>
+                        <div class="mt-3 mb-3 text-center detalhe">
+                            <a href="#" class="nav-link text-warning">ver mais detalhes</a>
+                        </div>
+                    </div>
+                    <div class="shine"></div>
+                </div>
+            </div>
+            <div class="col" data-aos="fade-up" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="utente-carta carta card h-100 carta-2">
+                    <img src="<?= IMAGENS ?>admin/admin.png" alt="Foto de Perfil" class="shadow-sm profile-picture">
+                    <div class="card-body content">
+                        <h5 class="card-title utente-nome">nome utente</h5>
+                        <div class="traco-2"></div>
+                        <div class="mt-3 mb-3 text-center detalhe">
+                            <a href="#" class="nav-link text-warning">ver mais detalhes</a>
+                        </div>
+                    </div>
+                    <div class="shine"></div>
+                </div>
+            </div>
+            <div class="col" data-aos="fade-up" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="utente-carta carta card h-100 carta-2">
+                    <img src="<?= IMAGENS ?>admin/admin.png" alt="Foto de Perfil" class="shadow-sm profile-picture">
+                    <div class="card-body content">
+                        <h5 class="card-title utente-nome">nome utente</h5>
+                        <div class="traco-2"></div>
+                        <div class="mt-3 mb-3 text-center detalhe">
+                            <a href="#" class="nav-link text-warning">ver mais detalhes</a>
+                        </div>
+                    </div>
+                    <div class="shine"></div>
+                </div>
+            </div>
+            <div class="col" data-aos="fade-up" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="utente-carta carta card h-100 carta-2">
+                    <img src="<?= IMAGENS ?>admin/admin.png" alt="Foto de Perfil" class="shadow-sm profile-picture">
+                    <div class="card-body content">
+                        <h5 class="card-title utente-nome">nome utente</h5>
+                        <div class="traco-2"></div>
+                        <div class="mt-3 mb-3 text-center detalhe">
+                            <a href="#" class="nav-link text-warning">ver mais detalhes</a>
+                        </div>
+                    </div>
             </div>
             <div class="col">
                 <div class="card h-100">
