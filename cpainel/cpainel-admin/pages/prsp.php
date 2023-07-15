15de9025cdee01e5806792043dfa948deba3bf72
Nzongo Pedro <nzongopedro3@gmail.com>
2023-07-15 16:51:34 +0100

    Página principal com o logotipo

---
+++ cpainel/cpainel-admin/pages/prsp.php
@@ -0,0 +1,3 @@
+<div class="text-center h-50" id="prsp" data-aos="fade-right" data-aos-transition="4000" data-aos-durantion="4000">
+    <img src="<?= IMAGENS ?>logo/logotipo1_principal.svg" alt="" class="img-fluid w-100" style="height:98vh; margin-top:-60px">
+</div>
