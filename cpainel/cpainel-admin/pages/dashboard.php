f6d530355836bf9271e7cf379d4ba5d05c78ad77
Nzongo Pedro <nzongopedro3@gmail.com>
2023-07-15 16:52:06 +0100

    Pagina separada do dashboard

---
+++ cpainel/cpainel-admin/pages/dashboard.php
@@ -0,0 +1,45 @@
+  <!-- SECÇÃO dasbh-->
+  <section class="card dash border-0 rounded-0 shadow-sm" id="dashboard">
+      <div class="card-body corpo-dash">
+          <!-- CARDS DE INFORMAçÃO -->
+          <div class="row">
+              <div class="col-3">
+                  <div class="shadow-sm alert alert-danger alert-dismissible alertas rounded-0" role="alert" data-aos="fade-left" data-aos-transition="1500" data-aos-durantion="2500">
+                      <div class="text-center">
+                          <i class="bi bi-people-fill alerta-icon"></i>
+                          <h5 class="alerta-titulo">Utentes</h5>
+                          <span class="alerta-num">10</span>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-3">
+                  <div class="shadow-sm alert alert-danger alert-dismissible alertas rounded-0" role="alert" data-aos="fade-left" data-aos-transition="2000" data-aos-durantion="2500">
+                      <div class="text-center">
+                          <i class="bi bi-person-lines-fill alerta-icon"></i>
+                          <h5 class="alerta-titulo">gestores</h5>
+                          <span class="alerta-num">10</span>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-3">
+                  <div class="shadow-sm alert alert-danger alert-dismissible alertas rounded-0" role="alert" data-aos="fade-left" data-aos-transition="2000" data-aos-durantion="2500">
+                      <div class="text-center">
+                          <i class="bi bi-bank2 alerta-icon"></i>
+                          <h5 class="alerta-titulo">postos</h5>
+                          <span class="alerta-num">10</span>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-3">
+                  <div class="shadow-sm alert alert-danger alert-dismissible alertas rounded-0" role="alert" data-aos="fade-left" data-aos-transition="2000" data-aos-durantion="2500">
+                      <div class="text-center">
+                          <i class="bi bi-person-video alerta-icon"></i>
+                          <h5 class="alerta-titulo">admin</h5>
+                          <span class="alerta-num">10</span>
+                      </div>
+                  </div>
+              </div>
+          </div> <!-- FIM CARDS DE INFORMAçÃO -->
+          <h5 class="card-title titulo-dash">dashboard</h5>
+      </div><!-- Fim card informação -->
+  </section><!-- Fim secção dash -->
