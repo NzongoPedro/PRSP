371538c88a7de99c8ea66da0b31668e70ab45fe7
Nzongo Pedro <nzongopedro3@gmail.com>
2023-07-15 16:52:29 +0100

    Página de visualização de utentes

---
+++ cpainel/cpainel-admin/pages/utentes.php
@@ -0,0 +1,49 @@
+    <!-- Gerenciando utentes -->
+    <div class="container">
+        <section class="utentes mt-2" id="utentes">
+            <div class="card" data-aos="fade-in" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="card-body">
+                    <h5 class="card-title text-muted">Postos</h5>
+                </div>
+        </section><!-- Fim utentes -->
+
+        <!-- Grelha de utentes -->
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a short card.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
