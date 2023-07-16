868ce169e70d3bebea0d887820a79bbd4a5c7f54
Nzongo Pedro <nzongopedro3@gmail.com>
2023-07-16 10:36:29 +0100

    Páginas de adm, gerenciamento

---
+++ cpainel/cpainel-admin/pages/admnistradores.php
@@ -0,0 +1,43 @@
+    <!-- Gerenciando adm -->
+    <div class="container">
+        <section class="mt-2 mb-3" id="adm">
+            <div class="carta-1 card rounded-0" data-aos="fade-in" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="card-body">
+                    <h5 class="card-title titulo-page">Gerenciar adm</h5>
+                </div>
+            </div>
+        </section><!-- Fim ADM -->
+        <!-- Linha de dados -->
+
+        <section class="adms">
+            <div class="carta-1 card rounded-0" data-aos="fade-in" data-aos-transition="2000" data-aos-durantion="2500">
+                <div class="card-body">
+                    <h5 class="card-title titulo-page">Cadastrar ADM</h5>
+                    <div class="row">
+                        <div class="col-4">
+                            <form action="">
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="floatingInputName" placeholder="Nome completo">
+                                    <label for="floatingInputName">nome</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="floatingInputTelefone" placeholder="9xx xxx xxx">
+                                    <label for="floatingInputTelefone">telefone</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                    <label for="floatingInput">e-mail</label>
+                                </div>
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                    <label for="floatingPassword">palavra-passe</label>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </section>
+    </div>
